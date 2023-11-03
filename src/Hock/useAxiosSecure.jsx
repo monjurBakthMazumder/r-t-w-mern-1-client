01dd7841cc8e2d941e300ea86b10fceb73cde5d3
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-04 05:10:53 +0600

    update

--- src/Hock/useAxiosSecure.jsx
+++ src/Hock/useAxiosSecure.jsx
@@ -4,8 +4,8 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://car-care-server-fawn.vercel.app",
-  baseURL: 'http://localhost:5000',
+  baseURL: "https://car-care-server-fawn.vercel.app",
+  // baseURL: 'http://localhost:5000',
   withCredentials: true,
 });
 const useAxiosSecure = () => {
