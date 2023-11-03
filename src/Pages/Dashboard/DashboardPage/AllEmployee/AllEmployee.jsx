dea5dc07be34ea6cf30083d48fcde731fbba8af9
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-03 19:04:15 +0600

    add pagination in all employee page

--- src/Pages/Dashboard/DashboardPage/AllEmployee/AllEmployee.jsx
+++ src/Pages/Dashboard/DashboardPage/AllEmployee/AllEmployee.jsx
@@ -5,10 +5,19 @@ import useAxiosSecure from "../../../../Hock/useAxiosSecure";
 
 const AllEmployee = () => {
   const [employees, setEmployees] = useState([]);
+  const [count, setCount] = useState(null);
+  const [itemPerPage, setItemPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
   const axiosSecure = useAxiosSecure();
   useEffect(() => {
-    axiosSecure.get("/employees").then((res) => setEmployees(res.data));
-  }, [axiosSecure]);
+    axiosSecure.get("/employees-count").then((res) => setCount(res.data.count));
+    axiosSecure
+      .get(`/employees?page=${currentPage}&size=${itemPerPage}`)
+      .then((res) => setEmployees(res.data));
+  }, [axiosSecure, currentPage, itemPerPage]);
+  const numberOfPages = Math.ceil(count / itemPerPage);
+  const pages = [...Array(numberOfPages).keys()];
+  console.log(pages);
   const handleDelete = (id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -32,10 +41,24 @@ const AllEmployee = () => {
       }
     });
   };
+  const handleItemParPageChange = (e) => {
+    setItemPerPage(Number(e.target.value));
+    setCurrentPage(0);
+  };
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNextPage = () => {
+    if (currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
   return (
     <div className="my-10 md:my-20 w-full">
       <h1 className="text-2xl md:text-3xl text-center text-primary underline font-bold mb-10">
-        Total Employees: {employees?.length}
+        Total Employees: {count}
       </h1>
       <div className="overflow-x-auto">
         <table className="table">
@@ -61,6 +84,43 @@ const AllEmployee = () => {
             ))}
           </tbody>
         </table>
+      </div>
+      <div className="flex flex-wrap justify-center items-center gap-2 my-10">
+        <button
+          onClick={handlePrevPage}
+          className={`px-2 py-1 border border-primary flex justify-center items-center gap-2 hover:bg-primary hover:text-white text-primary bg-transparent`}
+        >
+          Prev
+        </button>
+        {pages?.map((page) => (
+          <button
+            key={page}
+            onClick={() => setCurrentPage(page)}
+            className={`px-2 py-1 border border-primary flex justify-center items-center gap-2 hover:bg-primary hover:text-white ${
+              currentPage === page
+                ? "bg-primary text-white"
+                : "text-primary bg-transparent"
+            }`}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          onClick={handleNextPage}
+          className={`px-2 py-1 border border-primary flex justify-center items-center gap-2 hover:bg-primary hover:text-white text-primary bg-transparent`}
+        >
+          Next
+        </button>
+        <select
+          value={itemPerPage}
+          onChange={handleItemParPageChange}
+          className="select select-primary w-fit text-primary"
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+        </select>
       </div>
     </div>
   );
